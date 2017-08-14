64b3959dd584c11cc76d2cae214f299e5ee36775
Peter Bengtsson <mail@peterbe.com>
2017-08-14 14:44:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A work in progress.
+
+The goal is a bot that can automatically create Bugzilla attachment comments
+when new GitHub Pull Requests are created.
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return "See README"
+
+
+
+
+if __name__ == '__main__':
+    # XXX env DEBUG instead
+    app.run(debug=True, use_reloader=True)
