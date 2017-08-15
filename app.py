40d5eccb81552a1e2854699270a2e472c8d160e9
Peter Bengtsson <mail@peterbe.com>
2017-08-15 12:43:28 -0400

    fixes

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import sys
 import re
 import hmac
 import logging
-# import ipaddress
-# from pprint import pprint
 
 import requests
 from requests.adapters import HTTPAdapter
@@ -24,9 +22,6 @@ BUGZILLA_BASE_URL = config('BUGZILLA_BASE_URL', 'https://bugzilla.mozilla.org')
 # XXX perhaps we can use this:
 # https://mana.mozilla.org/wiki/display/WebDev/Bugzilla+Github+Bug+Closer+Account
 BUGZILLA_API_KEY = config('BUGZILLA_API_KEY')
-
-# Optional
-GHE_ADDRESS = config('GHE_ADDRESS', None)
 
 
 app = Flask(__name__)
@@ -57,7 +52,6 @@ def requests_retry_session(
     return session
 
 
-
 class ConfigurationError(ValueError):
     """when there's something wrong with the current config values"""
 
@@ -68,36 +62,6 @@ def postreceive():
 
     if request.method == 'GET':
         return "Yeah, it works but use POST\n"
-
-    # app.logger.debug('APP DEBUG')
-    # app.logger.info('APP INFO')
-    # app.logger.warning('APP WARNING')
-    # app.logger.error('APP ERROR')
-
-    # # Store the IP address of the requester
-    # print('request.remote_addr', repr(request.remote_addr))
-    # request_ip = ipaddress.ip_address(request.remote_addr)
-    #
-    # # If GHE_ADDRESS is specified, use it as the hook_blocks.
-    # if GHE_ADDRESS:
-    #     hook_blocks = [GHE_ADDRESS]
-    # # Otherwise get the hook address blocks from the API.
-    # else:
-    #     # XXX cache this
-    #     hook_blocks = requests.get(
-    #         'https://api.github.com/meta'
-    #     ).json()['hooks']
-    #
-    # # Check if the POST request is from github.com or GHE
-    # print('hook_blocks', hook_blocks)
-    # print('request_ip', repr(request_ip))
-    # for block in hook_blocks:
-    #     if ipaddress.ip_address(request_ip) in ipaddress.ip_network(block):
-    #         break  # the remote_addr is within the network range of github.
-    # else:
-    #     if request_ip != '127.0.0.1':
-    #         print('request_ip != 127.0.0.1')
-    #         abort(403)
 
     if request.headers.get('X-GitHub-Event') == 'ping':
         return {'msg': 'Hi!'}
@@ -121,18 +85,6 @@ def postreceive():
         abort(400)
 
     raw_payload = request.get_data()
-    logger.debug(f'raw_payload is {len(raw_payload)} bytes')
-    # print('raw_payload', len(raw_payload), repr(raw_payload[:50]))
-    # print('request.data:')
-    # print(type(request.get_data()))
-    # print(repr(request.get_data()))
-    # print('request.body:')
-    # print(type(request.body))
-    # print(repr(request.body))
-
-    # HMAC requires the key to be bytes, but data is string
-    # print('request.data', len(request.data))
-    # print('request.get_data()', len(request.get_data()))
     mac = hmac.new(
         GITHUB_WEBHOOK_SECRET.encode('utf-8'),
         msg=raw_payload,
@@ -144,11 +96,6 @@ def postreceive():
         abort(403)
 
     posted = json.loads(request.form['payload'])
-    # posted=payload
-    # from pprint import pprint
-    # logger.info(type(posted))
-    # logger.info(str(posted)[:1000])
-    # pprint(posted)
 
     if not posted.get('pull_request'):
         logger.warning(
@@ -183,13 +130,8 @@ def postreceive():
         logger.warning(f'Bug {bug_id!r} can not be found')
         abort(400)
 
-    # print("BUG_COMMENTS")
-    # print(repr(bug_comments))
-
     # loop over the current comments to see if there's already on
     for i, comment in enumerate(bug_comments):
-        # print('COMMENT')
-        # print(comment)
         if comment.get('is_obsolete'):
             continue
         if url in comment['text']:
@@ -204,14 +146,14 @@ def postreceive():
 
     diff_data = _get_diff_data(session, pull_request['diff_url']) or ''
 
-    comment = pull_request.get('description', '*no pull request description*')
+    comment = pull_request.get('description', '')
 
     response = requests.post(attachment_url, json={
         'ids': [bug_id],
         'summary': summary,
         'data': base64.b64encode(diff_data.encode('utf-8')).decode('utf-8'),
         'file_name': f'file_{pull_request_id}.txt',
-        # 'content_type': 'text/x-github-pull-request',
+        'content_type': 'text/x-github-pull-request',
         # 'content_type': 'text/plain',
         'comment': comment,
         'is_patch': 'true',
@@ -247,8 +189,6 @@ def _get_diff_data(session, url):
 def find_bug_comments(session, id):
     """Return true if the bug can be found"""
     # XXX should this use secure credentials??
-    # bug_url = f'{BASE_URL}/rest/bug/{bug_id}/comment'
-    # XXX Idea; it could
     bug_url = f'{BUGZILLA_BASE_URL}/rest/bug/{id}/comment'
     response = session.get(bug_url)
     print('bug_url', bug_url, response.status_code)
